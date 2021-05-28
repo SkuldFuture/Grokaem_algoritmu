6053a127b79b9741d298539d83dab2c09412c493
SkuldFuture <Vakadana@gmail.com>
2021-05-28 22:21:29 +0300

    Added a table of contents to the first example. added a second example.

--- hash_table/hash_t=dict.py
+++ hash_table/hash_t=dict.py
@@ -1,3 +1,4 @@
+# GROCERY LIST
 book = dict()
 
 book["apple"] = 0.67
@@ -6,3 +7,12 @@ book["avocado"] = 1.49
 
 print(book)  # {'apple': 0.67, 'milk': 1.49, 'avocado': 1.49}
 print(book["avocado"])  # 1.49
+
+# REFERENCE BOOK
+phone_book = {}
+
+phone_book["jenny"] = 8675309
+phone_book["emergency"] = 911
+
+print(phone_book)   # {'jenny': 8675309, 'emergency': 911}
+print(phone_book["jenny"])  # 8675309

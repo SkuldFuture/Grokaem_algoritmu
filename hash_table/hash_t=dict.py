75a3ee3b4e294ee4d0f8854622b2a4c09c590cdd
SkuldFuture <Vakadana@gmail.com>
2021-05-28 22:23:38 +0300

    Added a third example.

--- hash_table/hash_t=dict.py
+++ hash_table/hash_t=dict.py
@@ -14,5 +14,21 @@ phone_book = {}
 phone_book["jenny"] = 8675309
 phone_book["emergency"] = 911
 
-print(phone_book)   # {'jenny': 8675309, 'emergency': 911}
+print(phone_book)  # {'jenny': 8675309, 'emergency': 911}
 print(phone_book["jenny"])  # 8675309
+
+# ELIMINATION OF DUPLICATES
+voted = {}
+
+
+def check_voter(name):
+    if voted.get(name):
+        print("kick them out!")
+    else:
+        voted[name] = True
+        print("let them vote!")
+
+
+print(check_voter("tom"))
+print(check_voter("mike"))
+print(check_voter("mike"))
